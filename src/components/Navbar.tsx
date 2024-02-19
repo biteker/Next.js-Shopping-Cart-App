965ff11f527d61d6077275b304272e9ee49a3fa5
Huseyin Biteker <biteker@gmail.com>
2024-02-19 18:54:47 +0300

    "First run"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,7 +55,7 @@ function ResponsiveAppBar() {
             }}
             style={{ cursor: "pointer" }}
           >
-            My Ecommerce Site
+            Time To Eat
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
