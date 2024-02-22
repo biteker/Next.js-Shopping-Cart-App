0a4f2ae03288053faa14a92e1ed91529ba23132b
Huseyin Biteker <biteker@gmail.com>
2024-02-22 22:18:21 +0300

    Logo fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home({ products, categories }: Props) {
   return (
     <>
       <Head>
-        <title>Product List</title>
+        <title>Time To Eat</title>
         <meta
           name="description"
           content="Discover the best deals and shop for your favorite products at our online store. With a wide range of categories and brands, our eCommerce app makes shopping easy and convenient. Get free shipping on orders over $50 and enjoy hassle-free returns. Download our app now and start shopping!"
@@ -48,8 +48,7 @@ export async function getStaticProps() {
   // You can use any data fetching library and can also query the database directly.
   const products: Product[] = await getProducts();
   const categories: { id: string; description: string }[] = (await getCategories()).map(category => ({ id: category.id, description: category.description }));
-  console.log(categories);
-  
+
 
   // By returning { props: { products } }, the Home component
   // will receive `products` as a prop at build time in Production
